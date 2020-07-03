6c8664af323e7bd7e09eca79a4fbdf2917c123aa
Johnson <johnnyjp2070@gmail.com>
2020-07-03 11:17:18 +0530

    Updates

---
+++ precache-manifest.c75cd621635df1eab9a0f203ff2a74db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cc3a838a5c8299d50603b9c8defbac",
+    "url": "/mobionics-shopping-cart/index.html"
+  },
+  {
+    "revision": "e8429eb29d461e1dbdf3",
+    "url": "/mobionics-shopping-cart/static/css/2.b5dbb736.chunk.css"
+  },
+  {
+    "revision": "a3f39aadfc1aebbc403b",
+    "url": "/mobionics-shopping-cart/static/css/main.f48f426e.chunk.css"
+  },
+  {
+    "revision": "e8429eb29d461e1dbdf3",
+    "url": "/mobionics-shopping-cart/static/js/2.b4c92128.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobionics-shopping-cart/static/js/2.b4c92128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f39aadfc1aebbc403b",
+    "url": "/mobionics-shopping-cart/static/js/main.97a0040d.chunk.js"
+  },
+  {
+    "revision": "8cb4078abbf41ed13e56",
+    "url": "/mobionics-shopping-cart/static/js/runtime-main.5fd324c1.js"
+  },
+  {
+    "revision": "8f480f78d52d72c3985c82255a64265e",
+    "url": "/mobionics-shopping-cart/static/media/spinner.8f480f78.gif"
+  }
+]);
